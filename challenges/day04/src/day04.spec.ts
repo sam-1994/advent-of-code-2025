e5a8e4eb252061e657627b4a1528af0684a2dbd9
samuel-schober <samuel.schober@socialsynergy.de>
2025-12-04 11:47:54 +0100

    feat(day04): implement part 2

--- challenges/day04/src/day04.spec.ts
+++ challenges/day04/src/day04.spec.ts
@@ -1,9 +1,9 @@
 import {
   solve as solve1,
 } from './day04_1';
-// import {
-//   solve as solve2,
-// } from './day03_2';
+import {
+  solve as solve2,
+} from './day04_2';
 import { getInput } from './get-input';
 
 const inputExample = getInput(`${__dirname}/assets/example.txt`);
@@ -15,6 +15,9 @@ describe('Day 04', () => {
     });
   });
 
-  // describe('part 2', () => {
-  // })
+  describe('part 2', () => {
+    it('solves the example', () => {
+      expect(solve2(inputExample)).toBe(43);
+    });
+  })
 });
---
+++ challenges/day04/src/day04_2.ts
@@ -0,0 +1,44 @@
+import { Input } from './get-input';
+
+export function solve(input: Input): number {
+  let reachableRolls = 0;
+  let changeFound = false;
+  do {
+    changeFound = false;
+    for (let y = 0; y < input.length; y++) {
+      for (let x = 0; x < input[y].length; x++) {
+        if (input[y][x]) {
+          let adjacentRolls = 0;
+          const adjacentYStart = Math.max(0, y - 1);
+          const adjacentYEnd = Math.min(input.length - 1, y + 1);
+          const adjacentXStart = Math.max(0, x - 1);
+          const adjacentXEnd = Math.min(input[y].length - 1, x + 1);
+          for (
+            let adjacentY = adjacentYStart;
+            adjacentY <= adjacentYEnd;
+            adjacentY++
+          ) {
+            for (
+              let adjacentX = adjacentXStart;
+              adjacentX <= adjacentXEnd;
+              adjacentX++
+            ) {
+              if (adjacentY === y && adjacentX === x) {
+                continue;
+              }
+              if (input[adjacentY][adjacentX]) {
+                adjacentRolls++;
+              }
+            }
+          }
+          if (adjacentRolls < 4) {
+            reachableRolls++;
+            input[y][x] = false;
+            changeFound = true;
+          }
+        }
+      }
+    }
+  } while (changeFound);
+  return reachableRolls;
+}
